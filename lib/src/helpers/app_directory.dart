d7a97e577282392d94553bea6bf875fbdff9de0d
Agraj Parashar <agraj.parashar@unthinkable.co>
2023-07-20 10:15:36 +0530

    intial

---
+++ lib/src/helpers/app_directory.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+
+class AppDirectory {
+  static Future<String> getDocumentsDirectory() async {
+    final directory = Platform.isAndroid
+        ? await getExternalStorageDirectory()
+        : await getApplicationDocumentsDirectory();
+    return directory?.path ?? '';
+  }
+}
