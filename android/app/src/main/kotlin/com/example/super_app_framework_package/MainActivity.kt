3bbfc700ebc423f9e24866e233c1e2fdc4cfcbc8
Agraj Parashar <agraj.parashar@unthinkable.co>
2023-07-31 14:13:27 +0530

    intial

---
+++ android/app/src/main/kotlin/com/example/super_app_framework_package/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.super_app_framework_package
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- example/android/app/src/main/kotlin/com/example/super_app_framework_package_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.super_app_framework_package_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
